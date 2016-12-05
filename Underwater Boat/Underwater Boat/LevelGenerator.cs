5340b160fbd4ccb85df623bc5073a4051111ff1a
runeanderberg <rune.anderberg@lund.ntig.se>
2016-12-05 10:05:54 +0100

    added bottom generation

--- Underwater Boat/Underwater Boat/LevelGenerator.cs
+++ Underwater Boat/Underwater Boat/LevelGenerator.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -16,13 +14,14 @@ namespace Underwater_Boat
         public static Texture2D GenerateLevel(GraphicsDevice gD, int width, int height)
         {
             Texture2D level = new Texture2D(gD, width, height, false, SurfaceFormat.Color);
-
-            Color[,] col2D = new Color[level.Width, level.Height];
-
+            
             List<List<Point>> polygons = new List<List<Point>>
             {
-                GeneratePolygon(new Point(250, 250), 8)
+                GeneratePolygon(new Point(250, 250), 5),
+                GenerateBottom(width, height, 8)
             };
+
+            Color[,] col2D = new Color[level.Width, level.Height];
 
             for (int w = 0; w < width; w++)
             {
@@ -32,29 +31,30 @@ namespace Underwater_Boat
                     {
                         if (IsPointInPolygon(polygon, new Point(w, h)))
                         {
-                            col2D[w, h] = Color.Red;
+                            col2D[w, h] = Color.SeaGreen;
                         }
                     }
                 }
             }
 
-            foreach (var polygon in polygons)
-            {
-                foreach (var point in polygon)
-                {
-                    // drawing points for testing purposes
-                    col2D[point.X + 500, point.Y + 200] = Color.Red;
+            // Testcode
+            //foreach (var polygon in polygons)
+            //{
+            //    foreach (var point in polygon)
+            //    {
+            //        // drawing points for testing purposes
+            //        col2D[point.X + 500, point.Y + 200] = Color.Red;
 
-                    col2D[point.X + 500 - 1, point.Y - 1 + 200] = Color.Red;
-                    col2D[point.X + 500, point.Y - 1 + 200] = Color.Red;
-                    col2D[point.X + 500 + 1, point.Y - 1 + 200] = Color.Red;
-                    col2D[point.X + 500 + 1, point.Y + 200] = Color.Red;
-                    col2D[point.X + 500 + 1, point.Y + 1 + 200] = Color.Red;
-                    col2D[point.X + 500, point.Y + 1 + 200] = Color.Red;
-                    col2D[point.X + 500 - 1, point.Y + 1 + 200] = Color.Red;
-                    col2D[point.X + 500 - 1, point.Y + 200] = Color.Red;
-                }
-            }
+            //        col2D[point.X + 500 - 1, point.Y - 1 + 200] = Color.Red;
+            //        col2D[point.X + 500, point.Y - 1 + 200] = Color.Red;
+            //        col2D[point.X + 500 + 1, point.Y - 1 + 200] = Color.Red;
+            //        col2D[point.X + 500 + 1, point.Y + 200] = Color.Red;
+            //        col2D[point.X + 500 + 1, point.Y + 1 + 200] = Color.Red;
+            //        col2D[point.X + 500, point.Y + 1 + 200] = Color.Red;
+            //        col2D[point.X + 500 - 1, point.Y + 1 + 200] = Color.Red;
+            //        col2D[point.X + 500 - 1, point.Y + 200] = Color.Red;
+            //    }
+            //}
 
             Color[] col = new Color[level.Width*level.Height];
 
@@ -74,7 +74,7 @@ namespace Underwater_Boat
             return level;
         }
 
-        public static List<Point> GeneratePolygon(Point center, int iterations)
+        private static List<Point> GeneratePolygon(Point center, int iterations)
         {
             List<Point> points = new List<Point>();
 
@@ -95,7 +95,7 @@ namespace Underwater_Boat
                 for (int j = 0; j < points.Count; j++)
                 {
                     // To stop it from creating points to close to eachother
-                    if (new Vector2(points[i].X - points[i + 1].X, points[i].Y - points[i + 1].Y).Length() > 10)
+                    if (new Vector2(points[j].X - points[(j + 1) % points.Count].X, points[j].Y - points[(j + 1) % points.Count].Y).Length() > 10)
                     {
                         // Creates a point inbetween with super fancy math
                         var angle =
@@ -137,6 +137,52 @@ namespace Underwater_Boat
             return points;
         }
 
+        private static List<Point> GenerateBottom(int width, int height, int iterations)
+        {
+            Random rand = new Random();
+
+            List<Point> points = new List<Point>
+            {
+                new Point(0, rand.Next(height - 200, height)),
+                new Point(width, rand.Next(height - 200, height))
+            };
+
+            int length = 200;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                List<Point> tempList = new List<Point>();
+
+                for (int j = 0; j < points.Count - 1; j++)
+                {
+                    // To stop it from creating points to close to eachother
+                    if (new Vector2(points[j].X - points[j + 1].X, points[j].Y - points[j + 1].Y).Length() > 10)
+                    {
+                        var randPoint = new Point
+                        {
+                            X = points[j].X + (points[j + 1].X - points[j].X) / 2,
+                            Y = rand.Next((points[j].Y + (points[j + 1].Y - points[j].Y) / 2) - (int) Math.Round(length / Math.Pow(2, i)), (points[j].Y + (points[j + 1].Y - points[j].Y) / 2 + (int)Math.Round(length / Math.Pow(2, i))))
+                        };
+
+                        tempList.Add(randPoint);
+                    }
+                }
+
+                var index = 1;
+
+                foreach (var point in tempList)
+                {
+                    points.Insert(index, point);
+                    index += 2;
+                }
+            }
+
+            points.Insert(0, new Point(0, height));
+            points.Add(new Point(width, height));
+
+            return points;
+        }
+
         //public static bool IntersectsWithPoint(List<Point> points, Vector2 point)
         //{
         //    for (int i = 0; i < points.Count; i++)
@@ -151,7 +197,7 @@ namespace Underwater_Boat
         //    return true;
         //} 
 
-        public static bool IsPointInPolygon(List<Point> polygon, Point point)
+        private static bool IsPointInPolygon(List<Point> polygon, Point point)
         {
             bool isInside = false;
             for (int i = 0, j = polygon.Count - 1; i < polygon.Count; j = i++)
@@ -164,85 +210,5 @@ namespace Underwater_Boat
             }
             return isInside;
         }
-
-        #region IsPointInPolygon
-        // Return true if the point is in the polygon.
-        public static bool IsPointInPolygon(float x, float y, List<Point> polygon)
-        {
-            // Get the angle between the point and the
-            // first and last vertices.
-            int maxPoint = polygon.Count - 1;
-            float totalAngle = GetAngle(
-                polygon[maxPoint].X, polygon[maxPoint].Y,
-                x, y,
-                polygon[0].X, polygon[0].Y);
-
-            // Add the angles from the point
-            // to each other pair of vertices.
-            for (int i = 0; i < maxPoint; i++)
-            {
-                totalAngle += GetAngle(
-                    polygon[i].X, polygon[i].Y,
-                    x, y,
-                    polygon[i + 1].X, polygon[i + 1].Y);
-            }
-
-            // The total angle should be 2 * PI or -2 * PI if
-            // the point is in the polygon and close to zero
-            // if the point is outside the polygon.
-            return (Math.Abs(totalAngle) > 0.000001);
-        }
-
-        // Return the angle ABC.
-        // Return a value between PI and -PI.
-        // Note that the value is the opposite of what you might
-        // expect because Y coordinates increase downward.
-        public static float GetAngle(float Ax, float Ay, float Bx, float By, float Cx, float Cy)
-        {
-            // Get the dot product.
-            float dotProduct = DotProduct(Ax, Ay, Bx, By, Cx, Cy);
-
-            // Get the cross product.
-            float crossProduct = CrossProductLength(Ax, Ay, Bx, By, Cx, Cy);
-
-            // Calculate the angle.
-            return (float)Math.Atan2(crossProduct, dotProduct);
-        }
-
-        // Return the cross product AB x BC.
-        // The cross product is a vector perpendicular to AB
-        // and BC having length |AB| * |BC| * Sin(theta) and
-        // with direction given by the right-hand rule.
-        // For two vectors in the X-Y plane, the result is a
-        // vector with X and Y components 0 so the Z component
-        // gives the vector's length and direction.
-        public static float CrossProductLength(float Ax, float Ay,
-            float Bx, float By, float Cx, float Cy)
-        {
-            // Get the vectors' coordinates.
-            float BAx = Ax - Bx;
-            float BAy = Ay - By;
-            float BCx = Cx - Bx;
-            float BCy = Cy - By;
-
-            // Calculate the Z coordinate of the cross product.
-            return (BAx * BCy - BAy * BCx);
-        }
-
-        // Return the dot product AB · BC.
-        // Note that AB · BC = |AB| * |BC| * Cos(theta).
-        private static float DotProduct(float Ax, float Ay,
-            float Bx, float By, float Cx, float Cy)
-        {
-            // Get the vectors' coordinates.
-            float BAx = Ax - Bx;
-            float BAy = Ay - By;
-            float BCx = Cx - Bx;
-            float BCy = Cy - By;
-
-            // Calculate the dot product.
-            return (BAx * BCx + BAy * BCy);
-        }
-        #endregion
     }
 }
